8e9e21e41cb20db42c483750fd1f4795d596e973
Rashid Moin <rashidmoin@gmail.com>
2020-06-01 10:22:39 -0700

    DQN

---
+++ util_nav.py
@@ -0,0 +1,34 @@
+import torch
+
+import numpy as np
+import random
+from collections import namedtuple, deque
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+## Util function to randomly sample the expereinces tuples from memory
+def sample(memory, batch_size):
+    """Randomly sample a batch of experiences from memory."""
+    experiences = random.sample(memory, batch_size)
+
+    states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+    actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
+    rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+    next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+    dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+
+    return (states, actions, rewards, next_states, dones)
+
+## Another util function to implement Fixed Q-Target logic
+def update_Q_targets(local_model, target_model, tau):
+    """Soft update model parameters.
+    θ_target = τ*θ_local + (1 - τ)*θ_target
+
+    Params
+    ======
+        local_model (PyTorch model): weights will be copied from
+        target_model (PyTorch model): weights will be copied to
+        tau (float): interpolation parameter 
+    """
+    for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+        target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
